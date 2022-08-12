ea1599aacd47200b1bc3a453e468057ab5d40e9d
dionyysus <gizemcsk1@gmail.com>
2022-08-12 06:39:23 +0300

    project 1

---
+++ Day 16 (Conflicted copy from Gizem MacBook Air on 2022-08-03)/Project1/Project1/ViewController (Conflicted copy from Gizem MacBook Air on 2022-08-03).swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Gizem Coşkun on 2.08.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+        
+    }
+
+
+}
+
