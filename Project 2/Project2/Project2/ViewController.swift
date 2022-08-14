eb4700501c5b19123d184e3452f698d4989d1606
dionyysus <gizemcsk1@gmail.com>
2022-08-14 22:58:45 +0300

    project 2 completed

---
+++ Project 2/Project2/Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Gizem Coşkun on 14.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
