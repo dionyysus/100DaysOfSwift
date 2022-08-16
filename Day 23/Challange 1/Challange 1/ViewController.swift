fcfb545504dd344648f9ca05a01be1c6f33064f0
dionyysus <gizemcsk1@gmail.com>
2022-08-16 18:15:00 +0300

    challange 1 repeated

--- Day 23/Challange 1/Challange 1/ViewController.swift
+++ Day 23/Challange 1/Challange 1/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         title = "Flags Of Country"
-        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.prefersLargeTitles = false
         
         
         let fm = FileManager.default
@@ -22,6 +22,7 @@ class ViewController: UITableViewController {
         let items = try! fm.contentsOfDirectory(atPath: path)
         
         for item in items{
+            //The hasSuffix() method checks whether the string ends with the specified string or not.
             if item.hasSuffix("@2x.png"){
                 flags.append(item)
             }
@@ -30,18 +31,27 @@ class ViewController: UITableViewController {
         flags.sort()
        
     }
-    
+    // We take number of rows in table view
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return flags.count
+        
     }
     
+    //We create a cell for each table view row
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
         let cell = tableView.dequeueReusableCell(withIdentifier: "Country",for: indexPath)
+        
         let countryName = flags[indexPath.row].split(separator: "@")
+        
+        // set the text from the data model
         cell.textLabel?.text = String(countryName[0]).uppercased()
+        
         return cell
     }
     
+    // method to run when table view cell is tapped
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController{
@@ -50,6 +60,11 @@ class ViewController: UITableViewController {
         }
         
     }
+    //summary
+    //The view controller adopts the UITableViewDelegate and UITableViewDataSource protocols.
+    //The numberOfRowsInSection method determines how many rows there will be in the table view.
+    //The cellForRowAtIndexPath method sets up each row.
+    //The didSelectRowAtIndexPath method is called every time a row is tapped.
 
 
 }
