04f2cafe9f37a02164f33bb38d508075057bcbf0
dionyysus <gizemcsk1@gmail.com>
2022-08-21 10:11:57 +0300

    Monitoring page loads: UIToolbar and UIProgressView

--- Project 4/Project 4/Project 4/ViewController.swift
+++ Project 4/Project 4/Project 4/ViewController.swift
@@ -10,7 +10,8 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
     var webview: WKWebView!
-    
+    var progressView: UIProgressView!
+
     override func loadView() {
         webview = WKWebView()
         webview.navigationDelegate = self
@@ -21,6 +22,19 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webview.reload))
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        webview.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+
+    
         let url = URL(string: "https://www.hackingwithswift.com")!
         webview.load(URLRequest(url: url))
         webview.allowsBackForwardNavigationGestures = true
@@ -40,7 +54,11 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = URL(string: "https://" + action.title!)!
         webview.load(URLRequest(url: url))
     }
-    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webview.estimatedProgress)
+        }
+    }
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
     }
